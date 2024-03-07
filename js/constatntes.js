b40d1b3b5eb1d7d596e5541163033ec5dfb35578
Arnold Briñez <jgb199703@gmail.com>
2024-03-06 21:23:46 -0500

    proyecto con modulos terminado

--- js/constatntes.js
+++
@@ -1,9 +0,0 @@
-export const inputMascota = document.querySelector('#mascota'); 
-export const inputPropietario = document.querySelector('#propietario'); 
-export const inputTelefono = document.querySelector('#telefono'); 
-export const inputFecha = document.querySelector('#fecha'); 
-export const inputHora = document.querySelector('#hora'); 
-export const inputSintomas = document.querySelector('#sintomas'); 
-export const boton = document.querySelector('.form-group button');
-export const formulario = document.querySelector('#nueva-cita')
-export const contenedorCitashtml = document.querySelector('#citas')
